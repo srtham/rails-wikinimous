6eb7d0a31b9859cecda04aa30b5601a6f834ef2b
Sze Rong Tham <szerongtham@gmail.com>
2023-08-05 15:39:27 +0800

    index, show, new, edit, update action created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+puts 'Creating 10 fake movies...'
+10.times do
+  article = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Quote.jack_handey
+  )
+  article.save!
+end
+puts 'Finished!'
